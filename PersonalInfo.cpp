5af6aa1ba1cf8ef9f27f3798a90da4d37c61c374
Tony Le <>
2024-01-29 01:12:05 -0800

    Commented PersonalInfo

--- PersonalInfo.cpp
+++ PersonalInfo.cpp
@@ -1,23 +1,21 @@
 #include "PersonalInfo.h"
 
-// PersonalInfo class implementation
+// Default constructor
 PersonalInfo::PersonalInfo() {
   this->init(nullptr, nullptr);
 }
 
-PersonalInfo::PersonalInfo(char fullName[], char email[]) {
+// Constructor with arguments.
+PersonalInfo::PersonalInfo(const char fullName[], const char email[]) {
   this->init(fullName, email);
 }
 
-void PersonalInfo::init(char fullName[], char email[]) {
-  this->setFullName(fullName);
-  this->setEmail(email);
-}
-
+// Copy constructor.
 PersonalInfo::PersonalInfo(const PersonalInfo& personalInfo) {
   *this = personalInfo;
 }
 
+// Copy assignment operator.
 PersonalInfo& PersonalInfo::operator=(const PersonalInfo& anotherOne) {
   if (this != &anotherOne) {
     this->init(anotherOne.fullName, anotherOne.email);
@@ -26,28 +24,40 @@ PersonalInfo& PersonalInfo::operator=(const PersonalInfo& anotherOne) {
   return *this;
 }
 
+// Overloads the << operator to display PersonalInfo.
 ostream& operator<<(ostream& out, const PersonalInfo& personalInfo) {
   out << personalInfo.fullName << ';' << personalInfo.email;
   return out;
 }
 
+// Destructor
 PersonalInfo::~PersonalInfo() {
   freeDynamicArray<char>(this->fullName);
   freeDynamicArray<char>(this->email);
 }
 
+// Getter for fullName
 const char* PersonalInfo::getFullName() const {
   return fullName;
 }
 
-void PersonalInfo::setFullName(const char fullName[]) {
-  setCharArr(fullName, this->fullName);
-}
-
+// Getter for email
 const char* PersonalInfo::getEmail() const {
   return email;
 }
 
+// Setter for fullName
+void PersonalInfo::setFullName(const char fullName[]) {
+  setCharArr(fullName, this->fullName);
+}
+
+// Setter for email
 void PersonalInfo::setEmail(const char email[]) {
   setCharArr(email, this->email);
+}
+
+// Initializes the PersonalInfo object.
+void PersonalInfo::init(const char fullName[], const char email[]) {
+  this->setFullName(fullName);
+  this->setEmail(email);
 }
--- PersonalInfo.h
+++ PersonalInfo.h
@@ -3,43 +3,96 @@
 
 #include "myUtil.h"
 
+/**
+ * This class represents the personal information of a customer.
+ */
 class PersonalInfo {
   private:
     char* fullName = nullptr;
     char* email = nullptr;
 
   public:
-    // Default constructor
+    /**
+     * Default constructor
+     */
     PersonalInfo();
 
-    // Constructor with arguments
-    PersonalInfo(char fullName[], char email[]);
+    /**
+     * Constructor with arguments.
+     *
+     * @param fullName The full name of the customer.
+     * @param email The email address of the customer.
+     */
+    PersonalInfo(const char fullName[], const char email[]);
 
-    // Copy constructor
+    /**
+     * Copy constructor.
+     *
+     * @param personalInfo The personal info to copy.
+     */
     PersonalInfo(const PersonalInfo&);
 
-    // Copy assignment operator
+    /**
+     * Copy assignment operator.
+     *
+     * @param anotherOne The personal info to copy.
+     * @return The copied personal info.
+     */
     PersonalInfo& operator=(const PersonalInfo& anotherOne);
 
+    /**
+     * Overloads the << operator to display PersonalInfo.
+     *
+     * @param[in, out] out The output stream to which the PersonalInfo will be
+     *                 written.
+     * @param          personalInfo The PersonalInfo to display.
+     */
     friend ostream& operator<<(ostream& out, const PersonalInfo& personalInfo);
 
-    // Destructor
+    /**
+     * Destructor
+     */
     ~PersonalInfo();
 
-    // Getter for fullName
+    /**
+     * Getter for fullName
+     *
+     * @return The full name of the customer.
+     */
     const char* getFullName() const;
 
-    // Getter for email
+    /**
+     * Getter for email
+     *
+     * @return The email address of the customer.
+     */
     const char* getEmail() const;
 
   private:
-    // Setter for fullName
+    /**
+     * Setter for fullName
+     *
+     * @param fullName The full name of the customer.
+     * @return void
+     */
     void setFullName(const char fullName[]);
 
-    // Setter for email
+    /**
+     * Setter for email
+     *
+     * @param email The email address of the customer.
+     * @return void
+     */
     void setEmail(const char email[]);
 
-    void init(char fullName[], char email[]);
+    /**
+     * Initializes the PersonalInfo object.
+     *
+     * @param fullName The full name of the customer.
+     * @param email The email address of the customer.
+     * @return void
+     */
+    void init(const char fullName[], const char email[]);
 };
 
 #endif
